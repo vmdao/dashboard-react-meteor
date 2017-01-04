6a308b248054d074e8ce0851432a4f04f69f42f5
Minh Đạo Vũ <Minh o V>
2017-01-04 15:20:24 +0700

    7

--- server/main.js
+++ server/main.js
@@ -1,7 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { ReactRouterSSR } from 'meteor/rubix:reactrouter:react-router-ssr';
 import './setup-plugins';
-import {LogoCategories} from '../imports/api/LogoCategories';
+import { LogoCategories } from '../imports/api/LogoCategories';
 import routes from '../imports/routes';
 
 Meteor.startup(() => {
@@ -18,20 +18,21 @@ Meteor.startup(() => {
   } else {
     console.log('--------------------RUN DEVELOPMENT--------------------')
   }
-  
-  if (Meteor.isServer) {
-      Meteor.publish("userStatus", function() {
-        return Meteor.users.find({ "status.online": true });
-      });
 
-      Meteor.publish("users", function() {
-        return Meteor.users.find();
-      });
-    
-      Meteor.publish("users.alalytics", function() {
-        return VisitTracker.visits.find();
-      });
+  if (Meteor.isServer) {
+    Meteor.publish("userStatus", function () {
+      return Meteor.users.find({ "status.online": true });
+    });
+
+    Meteor.publish("users", function () {
+      return Meteor.users.find();
+    });
+
+    Meteor.publish("users.alalytics", function () {
+      return VisitTracker.visits.find();
+    });
   }
+  console.log(WebApp)
   WebApp.addHtmlAttributeHook(function () {
     return {
       "dir": "ltr",
